0a5203efe529595745f84479378fc6f7ce3416e7
jlee-cq <j.lee@caraquri.com>
2023-03-10 11:30:07 +0900

    액티비티에서 실행 확인 완료 1. 거의 같은 코드를 쓰는데 어떻게 잘 통합 시킬지 고민중

--- PermissionCatcherLibrary/src/main/java/com/easyo/permissioncatcherlibrary/PermissionCatcher.kt
+++ PermissionCatcherLibrary/src/main/java/com/easyo/permissioncatcherlibrary/PermissionCatcher.kt
@@ -1,57 +1,43 @@
 package com.easyo.permissioncatcherlibrary
 
-import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.content.ContextCompat
-import java.lang.ref.WeakReference
 
-class PermissionCatcher(activityRef: WeakReference<AppCompatActivity>) {
-    private val activity = activityRef.get()
-
-    companion object {
-        const val PERMISSION_CAMERA = Manifest.permission.CAMERA
-        const val PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE
-    }
-
+class PermissionCatcher(private val activity: AppCompatActivity) {
     private var permissionArray: Array<String>? = null
     private var allPermissionGranted: (() -> Unit)? = null
     private var permissionDenied: ((List<String>) -> Unit)? = null
     private var permissionExplained: ((List<String>) -> Unit)? = null
 
     private val permissionRequestLauncher =
-        activity?.let {
-            activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
-                val deniedPermissionList = permissions.filter { !it.value }.map { it.key }
-                when {
-                    deniedPermissionList.isNotEmpty() -> {
-                        val map = deniedPermissionList.groupBy { permission ->
-                            if (shouldShowRequestPermissionRationale(activity, permission)) DENIED else EXPLAINED
-                        }
-                        map[DENIED]?.let {
-                            println("my DENIED: ${map[DENIED]}")
-                            // 단순히 권한이 거부 되었을 때
-                            permissionDenied?.invoke(it)
-                        }
-                        map[EXPLAINED]?.let {
-                            println("my EXPLAINED: ${map[EXPLAINED]}")
-                            // 권한 요청이 완전히 막혔을 때(주로 앱 상세 창 열기)
-                            permissionExplained?.invoke(it)
-                        }
+        activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val deniedPermissionList = permissions.filter { !it.value }.map { it.key }
+            when {
+                deniedPermissionList.isNotEmpty() -> {
+                    val map = deniedPermissionList.groupBy { permission ->
+                        if (shouldShowRequestPermissionRationale(activity, permission)) DENIED else EXPLAINED
                     }
-                    else -> {
-                        // 모든 권한이 허가 되었을 때
-                        allPermissionGranted?.invoke()
+                    map[DENIED]?.let {
+                        println("my DENIED: ${map[DENIED]}")
+                        // 단순히 권한이 거부 되었을 때
+                        permissionDenied?.invoke(it)
                     }
+                    map[EXPLAINED]?.let {
+                        println("my EXPLAINED: ${map[EXPLAINED]}")
+                        // 권한 요청이 완전히 막혔을 때(주로 앱 상세 창 열기)
+                        permissionExplained?.invoke(it)
+                    }
+                }
+                else -> {
+                    // 모든 권한이 허가 되었을 때
+                    allPermissionGranted?.invoke()
                 }
             }
         }
-
-
-    private var onPermissionResult: (Boolean) -> Unit = {}
 
     fun setPermissions(permissionArray: Array<String>): PermissionCatcher {
         this.permissionArray = permissionArray
@@ -73,18 +59,13 @@ class PermissionCatcher(activityRef: WeakReference<AppCompatActivity>) {
         return this
     }
 
-    fun requestPermission(permission: String, onResult: (Boolean) -> Unit) {
-        onPermissionResult = onResult
+    fun requestPermission(allPermissionGranted: (() -> Unit)? = null) {
+        this.allPermissionGranted = allPermissionGranted
+        Log.d("my", "requested permissions: ${permissionArray?.size}")
 
-        activity?.let {
-            if (ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED) {
-                // Permission is already granted
-                onPermissionResult(true)
-            } else {
-                // Permission is not granted, request it
-                permissionRequestLauncher?.launch(permissionArray)
-            }
-        } ?: run { Log.e(this@PermissionCatcher.javaClass.simpleName, "activity WeakReference value is null") }
+        permissionArray?.filter {
+            ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_DENIED
+        }?.toTypedArray().also { permissionRequestLauncher.launch(it) }
     }
 }
 
--- app/src/main/java/com/easyo/permissioncatcher/MainActivity.kt
+++ app/src/main/java/com/easyo/permissioncatcher/MainActivity.kt
@@ -2,47 +2,57 @@ package com.easyo.permissioncatcher
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.easyo.permissioncatcherlibrary.PermissionCatcher
+import androidx.databinding.DataBindingUtil
+import com.easyo.permissioncatcher.databinding.ActivityMainBinding
+import com.easyo.permissioncatcherlibrary.*
 import com.easyo.permissioncatcherlibrary.TestLibrary.makeToast
-import com.easyo.permissioncatcherlibrary.extension.setPermissionCatcherResultLauncher
-import java.lang.ref.WeakReference
 
 class MainActivity : AppCompatActivity() {
-    private val permissionRequest = setPermissionCatcherResultLauncher(
-        allGranted = {
-            // 모든 권한이 확인되어 있을 때
-            makeToast(this, "모든 권한 얻어져 있음")
-        },
-        denied = {
-            // 1개라도 허락되지 않은 권한이 있을 때
-            makeToast(this, "허락되지 않은 권한 있음")
-        },
-        explained = {
-            // 완전히 거부된 권한이 있을 경우
-            makeToast(this, "완전 거부된 권한 있음")
+    private lateinit var binding: ActivityMainBinding
+    private val permissionCatcher = PermissionCatcher(this)
+        .setPermissions(
+            arrayOf(
+                PERMISSION_CAMERA,
+                PERMISSION_LOCATION_COARSE,
+                PERMISSION_STORAGE_READ,
+                PERMISSION_STORAGE_WRITE,
+                PERMISSION_PHONE_CALL,
+                PERMISSION_PHONE_READ_LOG,
+                PERMISSION_SMS_READ,
+                PERMISSION_CALENDAR_READ
+            )
+        )
+        .setAllPermissionGranted {
+            makeToast(this, "모든 권한 통과1")
+            Log.d("my", "모든 권한 통과1")
         }
-    )
+        .setPermissionDenied {
+            makeToast(this, "거부된 권한: $it")
+            Log.d("my", "거부된 권한: $it")
+
+        }
+        .setPermissionExplained {
+            makeToast(this, "완전 거부된 권한: $it")
+            Log.d("my", "완전 거부된 권한: $it")
+        }
+
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    private val permissionCatcher = PermissionCatcher(WeakReference(this))
-        .setPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS))
-        .setAllPermissionGranted {
-
-        }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        checkEssentialPermission()
-    }
-
-    private fun checkEssentialPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            permissionRequest.launch(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+        Log.d("my", "onCreate")
+        binding.checkPermission.setOnClickListener {
+            permissionCatcher.requestPermission {
+                makeToast(this, "모든 권한 통과2")
+                Log.d("my", "모든 권한 통과2")
+            }
         }
+
+
     }
 }
